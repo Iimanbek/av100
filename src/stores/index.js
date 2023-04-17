3a06e7c6b59f5e6e0773bd05b75be08fae043a75
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-04-17 16:39:58 +0600

    done

--- src/stores/index.js
+++ src/stores/index.js
@@ -8,6 +8,8 @@ export const useIndexStore = defineStore("index", {
     user_data: "",
     response: null,
     user_num: "",
+    xhr_data: "",
+    text: "",
   }),
   actions: {
     async getUserAccaunt() {
@@ -26,20 +28,38 @@ export const useIndexStore = defineStore("index", {
       );
       this.user_data = await this.response.data;
     },
-    async createUserAcc() {
-      const res = await axios.post(
-        "https://api.av100.ru/v3/register",
-        {
-          login: "837438478",
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "X-Api-Key": "8bcfb6e1-4fa8-4fae-872c-a435bbdbe8d9",
-          },
+    createUserAcc() {
+      var xhr = new XMLHttpRequest();
+      var json = JSON.stringify({
+        login: "0501099966",
+      });
+      xhr.open("POST", "https://api.av100.ru/v3/register", true);
+      function setHeaders(headers) {
+        for (let key in headers) {
+          xhr.setRequestHeader(key, headers[key]);
         }
-      );
-      console.log(res);
+      }
+      setHeaders({
+        "Content-type": "application/json",
+        "X-Api-Key": "8bcfb6e1-4fa8-4fae-872c-a435bbdbe8d9",
+      });
+      xhr.onload = function () {
+        if (this.status == 200) {
+          console.log("Worked");
+        } else {
+          console.log(this.status);
+        }
+      };
+      xhr.onerror = function () {
+        console.log("Request Error");
+      };
+      xhr.onreadystatechange = "...";
+
+      // Отсылаем объект в формате JSON и с Content-Type application/json
+      // Сервер принимает Content-Type и раскодирововает
+      xhr.send(json);
+      this.xhr_data = xhr;
+      console.log(this.xhr_data);
     },
     async save_profile() {},
   },
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -4,6 +4,9 @@
     <v-sheet width="300" class="mx-auto">
       <v-form fast-fail @submit.prevent>
         <v-text-field v-model="indexStore.login_data" label="Login" :rules="LoginRules"></v-text-field>
+        <div v-show="indexStore">
+          <p class="notification"></p>
+        </div>
         <v-btn @click="registr" color="green" type="submit" block class="mt-2">Submit</v-btn>
       </v-form>
     </v-sheet>
@@ -13,9 +16,15 @@
 import { mapStores } from 'pinia';
 import { useIndexStore } from '../stores/index'
 export default {
+  data() {
+    return {
+      user_text: ''
+    }
+  },
   methods: {
     registr() {
       this.indexStore.createUserAcc()
+      console.log(this.indexStore.xhr_data.status);
     }
   },
   mounted() {
@@ -25,4 +34,15 @@ export default {
   }
 }
 </script>
-<style lang="css"></style>
+<style lang="css">
+.login_wrapper {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+.notification {
+  color: red;
+  text-align: center;
+}
+</style>
